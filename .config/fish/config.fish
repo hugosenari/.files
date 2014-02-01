1f49c3de98286d3c21a81c251730c140b25356ba
hugosenari <hugosenari@gmail.com>
2014-02-01 14:04:19 -0200

    rename

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,8 @@ set __fish_git_prompt_char_stashstate '%'
 set __fish_git_prompt_char_upstream_ahead '>'
 set __fish_git_prompt_char_upstream_behind '<'
 
+set PATH $PATH /home/hugosenari/.local/bin
+
 
 function fish_right_prompt
         set last_status $status
