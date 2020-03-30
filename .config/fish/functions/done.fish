bc4f3d4ff6a37af073c6981e4d38ca324c9e1369
hugosenari <hugosenari@gmail.com>
2020-03-30 18:57:08 -0300

    feat(.config/fish/functions): #GH-1_090100836 done task
     fish alias to git

---
+++ .config/fish/functions/done.fish
@@ -0,0 +1,6 @@
+# Defined in - @ line 1
+function done --wraps='parent; git merge task' --description 'alias done "parent; git merge task"'
+  set CURRENT (pwb)
+  parent
+  git merge $CURRENT;
+end
